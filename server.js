cf99494ec6515a40c644f3299f90d36c747c1e44
Pete Fullen <pfullen.code@gmail.com>
2017-12-30 12:13:05 -0500

    Added home page with styling

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var exphbs = require("express-handlebars");
+var serveStatic = require('serve-static')
+
 
 
 
@@ -15,6 +18,14 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+//app.use(serveStatic(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname + '/public')));
 
 //  Data Objects here (Data)
 // =======================================
@@ -31,7 +42,9 @@ app.get("/", function(req, res ) {
 	res.sendFile(path.join(__dirname, "./public/home.html"));
 });
 
-
+app.get("/survey", function(req, res) {
+	res.sendFile(path.join(__dirname, "./public/survey.html"));
+});
 
 
 // Starts the server to begin listening
